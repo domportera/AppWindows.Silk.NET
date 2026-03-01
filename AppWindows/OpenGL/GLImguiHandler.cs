9799af3ef051cee1515cce3cff176259e0ed5f11
dom <domportera@proton.me>
2026-03-01 13:20:33 -0500

    move to subfolder

---
+++ AppWindows/OpenGL/GLImguiHandler.cs
@@ -0,0 +1,59 @@
+using System.Text;
+using ImGuiWindows;
+using Silk.NET.Core.Contexts;
+using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.SDL;
+using Silk.NET.Windowing;
+
+namespace SilkWindows;
+
+public sealed class GLImguiHandler : IImguiImplementation
+{
+    public GLImguiHandler(string title, GL graphicsContext, IWindow window, IInputContext inputContext)
+    {
+        Title = title;
+        _graphicsContext = graphicsContext;
+        _window = window;
+        _inputContext = inputContext;
+    }
+
+    public bool StartImguiFrame(float deltaSeconds)
+    {
+        try
+        {
+            _imguiController!.Update(deltaSeconds);
+            return true;
+        }
+        catch (SdlException e)
+        {
+            var log = e.Message.Contains("HID") ? e.Message : e.ToString();
+            Console.Error.WriteLine($"{nameof(SdlException)}: {log}");
+            return false;
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    }
+
+    public void EndImguiFrame()
+    {
+        _imguiController!.Render();
+    }
+
+    public void Dispose()
+    {
+        _imguiController!.Dispose();
+    }
+
+    public string Title { get; }
+
+    public IntPtr InitializeControllerContext(Action onConfigureIO)
+    {
+        _imguiController = new ImGuiController(gl: _graphicsContext, _window, _inputContext, null, onConfigureIO);
+        return _imguiController.Context;
+    }
+
+    private ImGuiController? _imguiController;
+    private readonly GL _graphicsContext;
+    private readonly IWindow _window;
+    private readonly IInputContext _inputContext;
+}
---
+++ AppWindows/SilkWindowProvider.cs
@@ -0,0 +1,23 @@
+﻿using ImGuiWindows;
+using Silk.NET.Windowing;
+using SilkWindows.OpenGL;
+
+namespace SilkWindows;
+
+//https://github.com/dotnet/Silk.NET/blob/main/examples/CSharp/OpenGL%20Tutorials/Tutorial%201.1%20-%20Hello%20Window/Program.cs
+public sealed class SilkWindowProvider : IImguiWindowProvider
+{
+    public SilkWindowProvider()
+    {
+    }
+
+    public object RenderContextLock { get; } = new();
+
+    // todo - implement updating font pack during window update
+    public void SetFonts(FontPack fontPack) => FontPack = fontPack;
+
+    // todo - multi-backend
+    public IWindowImplementation CreateWindow(in WindowOptions options, ImGuiWindow? parent) => new GLWindow(options, parent);
+
+    public FontPack? FontPack { get; private set; }
+}
